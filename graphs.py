9e372c619e29443470e7276ee01fa0d3fc52f6b1
Bruno <brunosandrade21@gmail.com>
2022-11-14 22:54:36 -0300

    add script for new simulations

--- graphs.py
+++ graphs.py
@@ -2,6 +2,12 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import warnings
+
+params = {'mathtext.default': 'regular' }     
+plt.rcParams["font.family"] = "courier new"     
+plt.rcParams.update(params)
+warnings.filterwarnings('ignore')
 
 
 def plot():
@@ -26,28 +32,37 @@ def plot():
     plt.tight_layout()
     plt.show()
 
-def heatmap():
-    vac = 85  # percentage vaccinated
-    v = 6  # vaccine
-    vac_d = {0: 'No Vaccination', 1: 'AstraZeneca', 2: 'CoronaVac', 4: 'Janssen', 6: 'Pfizer', 8: 'BR Vaccines'}
-    s = 0  # state
+def heatmap(v, vac, s):
+    fig, ax = plt.subplots(1, 1, figsize=(8.8, 5.7))
+    vac_d = {0: 'No Vaccination', 1: 'AstraZeneca', 2: 'CoronaVac', 4: 'Janssen', 6: 'Pfizer', 8: 'Vaccines Used in Brazil'}
     state = ['Infectious', 'Hospitalized', 'Vaccinated']
     irange = range(100, -1, -5)
     df = [pd.read_csv(f'newsim/v{str(v)}-{vac}/i{str(iso)}/epiPrev.csv') for iso in irange]
     inf = [d[state[s]].to_numpy() for d in df]
-    titles = [f'{i}%' for i in range(100, -1, -5)]
+    titles = [f'{i}' for i in range(100, -1, -5)]
     inf = pd.DataFrame(inf, titles)
-    sns.heatmap(inf)
-    plt.title(f'$P_{{vac}}=0.85$ - {vac_d[v]}')
-    plt.xlabel('Time(days)')
-    plt.ylabel('$P_{iso}$')
+    sns.heatmap(inf, cmap='flare_r')
+    ax.set_xticks(np.arange(0, 401, 50))
+    ax.set_xticklabels((c for c in np.arange(0, 401, 50)), rotation=0)
+    cbar = ax.collections[0].colorbar
+    cticks = [float(t.get_text().replace('−','-')) for t in cbar.ax.get_yticklabels()]
+    cbar.ax.set_yticklabels(map(lambda s: f'{round(s*100, 1)}%', cticks))
+    ax.set_xlabel('Time(days)')
+    plt.title(f'$P_{{vac}}={vac}\%$ - {vac_d[v]}')
+    plt.ylabel('$P_{iso} (\%)$')
     plt.tight_layout()
-    plt.show()
+    plt.savefig(f'heatmaps/v{v}_{vac}_{state[s][:3].lower()}.png')
+    # plt.show()
 
 
 def main():
-    # heatmap()
-    plot()  # normal_plot
+    # plot()  # normal_plot
+    vac = 85
+    state = 0
+    for v in (0, 1, 2, 4, 6, 8):
+        print(f'vac {v} - {vac}%... ', end='')
+        heatmap(v, vac, state)
+    print('\nDone!')
     
 
 if __name__ == '__main__':
--- run.sh
+++ run.sh
@@ -29,7 +29,6 @@ do
         mv results newsim/v$v-$vac/i$iso
     done
 done
-com
 
 for iso in 0 50
 do
@@ -48,6 +47,33 @@ do
 
             rm -rf newsim2/i$iso/v$v/v$vac
             mv results newsim2/i$iso/v$v/v$vac
+        done
+    done
+done
+com
+
+for v in 0 1 2 4 6 8
+do
+    for vac in 0 25 50 75 100
+    do
+        for((iso=0;iso<=100;iso+=5))
+        do
+            if ([ $v -eq 0 ] && [ $vac -gt 0 ]) || ([ $vac -eq 0 ] && [ $v -gt 0 ])
+            then
+                break
+            fi
+
+            mkdir -p results
+            mkdir -p newsim3/v$v/$vac/
+
+            mpirun -np 8 ./covid.x $v 1 $iso 0 $vac 0
+            mv InfPrevalence.csv results/infPrev.csv
+            mv epidemicsprevalence.csv results/epiPrev.csv
+            mv InfIncidence.csv results/infInc.csv
+            mv epidemicsincidence.csv results/epiInc.csv
+
+            rm -rf newsim3/v$v/$vac/i$iso
+            mv results newsim3/v$v/$vac/i$iso
         done
     done
 done
